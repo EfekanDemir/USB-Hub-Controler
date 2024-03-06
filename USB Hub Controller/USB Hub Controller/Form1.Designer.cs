f9074d4fd9fe7e212b4bee2dae6ae6a5db5ba3c2
EfekanDemir <efekandemir05@gmail.com>
2024-03-06 15:06:31 +0300

    USB Hub Controller
    Windows'ta USB Hub'larını aktif etmek ve devre dışı bırakmak için geliştirildi.

---
+++ USB Hub Controller/USB Hub Controller/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace USB_Hub_Controller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notification = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(80, 130);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(129, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Enable USB Port";
+            this.radioButton1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(80, 155);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(133, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Disable USB Port";
+            this.radioButton2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noto Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(36, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "USB Enable|Disable App";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // notification
+            // 
+            this.notification.Icon = ((System.Drawing.Icon)(resources.GetObject("notification.Icon")));
+            this.notification.Text = "notification";
+            this.notification.Visible = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Himalaya", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(4, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Please reboot your system after using the program";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 303);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "USB Controller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notification;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ USB Hub Controller/USB Hub Controller/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace USB_Hub_Controller
+{
+    public partial class Form1 : Form
+    {
+        bool control=false; //kontrol değişkeni
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                control = true;
+                ExecuteDevconCommand("enable", "USB\\*"); //aktif etme
+            }
+            else if (radioButton2.Checked)
+            {
+                ExecuteDevconCommand("disable", "USB\\*");// devre dışı bırakma
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close(); //İptal tuşu
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (IsUserAdmin() == false)
+            {
+                //adminlik sorgusu sonuç mesajı
+                MessageBox.Show("Uygulamayı yönetici olarak çalıştırmalısınız, aksi takdirde değişiklik yapamazsınız !!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+        }
+        #region devcon.exe işlemleri
+        private void ExecuteDevconCommand(string action, string deviceFilter)
+        {
+            // ProcessStartInfo sınıfını kullanarak yeni bir işlem başlatmak için bilgi oluşturma kısmı
+            ProcessStartInfo processInfo = new ProcessStartInfo
+            {
+                FileName = "\"C:\\Program Files (x86)\\Windows Kits\\10\\Tools\\10.0.22621.0\\x64\\devcon.exe\"",
+                Arguments = $"{action} {deviceFilter}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            // Process sınıfını kullanarak yeni bir işlem oluşturma
+            using (Process process = new Process { StartInfo = processInfo })
+            {
+                process.Start();
+                process.WaitForExit();
+
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                #region bilgilendirme ve hata mesajları
+                if (!string.IsNullOrEmpty(output))
+                {
+                    MessageBox.Show(output, "Bilgilendirme Mesajı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    notification.BalloonTipTitle = "Bilgilendirme";
+                    if (control == true)
+                    {
+                        notification.BalloonTipText = "USB HUB'ları aktif edildi.";
+                    }
+                    else
+                    {
+                        notification.BalloonTipText = "USB HUB'ları devre dışı bırakıldı.";
+                    }
+                    notification.Icon = SystemIcons.Information;
+                    notification.ShowBalloonTip(3000);
+                   
+                }
+                if (!string.IsNullOrEmpty(error))
+                    MessageBox.Show(error, "Hata Mesajı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                #endregion
+            }
+        }
+        #endregion
+        #region Adminlik Sorgusu
+        [DllImport("shell32")]
+        static extern bool IsUserAnAdmin();
+
+        public static bool IsUserAdmin() 
+        {
+            bool isAdmin = IsUserAnAdmin();
+            return isAdmin;
+        }
+        #endregion 
+    }
+}
